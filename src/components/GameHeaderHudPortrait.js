7dccdbd80ce335cd7dce197e1e68886cc0c564cc
RomainRoy83 <romain.roy.83@gmail.com>
2022-06-15 16:02:08 +0200

    First version of Game HUD

---
+++ src/components/GameHeaderHudPortrait.js
@@ -0,0 +1,11 @@
+import Archer from '../assets/img/archer.png'
+
+const GameHeaderHudPortrait = () => {
+  return (
+    <div className='gameHeaderHudPortrait'>
+      <img src={Archer} className='portrait' />
+    </div>
+  )
+}
+
+export default GameHeaderHudPortrait
