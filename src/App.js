9cbf39348f6579300c333bdbfe8f566a237efe4f
flora cesarini <flo.cesarini@gmail.com>
2022-06-16 10:03:50 +0200

    usable dice w/ creation of the GameOver page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg'
+import GameStoryBattleDice from './components/GameStoryBattleDice'
+import GameOver from './components/GameOver'
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.yozz
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn Reactsccc
-        </a>
-      </header>
+      {/* <GameStoryBattleDice /> */}
+      <GameOver />
     </div>
   )
 }
