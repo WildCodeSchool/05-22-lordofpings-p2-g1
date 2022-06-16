6252c1e70e38dc34e44c8ac282d07cd72c4d216c
Vincent BRIAND <vincent-briand_student2022@wilder.school>
2022-06-16 11:29:32 +0200

    small fix

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,30 +1,33 @@
-import { React, useState } from 'react'
+import { React, useState, useEffect } from 'react'
 import GameTavern from '../components/GameTavern'
 import tavernImg from '../assets/img/background/tavern.svg'
+// import quests from '../assets/json/frame.json'
 
 const Game = () => {
   const [page, setPage] = useState(-2) // ID de la page en cours (Sommaire au dessous)
-  // 0+ = Game
-  // -1 = Other to Game
-  // -2 = Tavern
+  // 0+ = Game Story
+  // -1 = Game [Other]
+  // -2 = Game Tavern
   // -3 = Game Over
   // -4 = Game Won
-  // -5 = Game Launch [Setup]
-  const [quest, setQuest] = useState(0) // ID de la quête en cours
-  const [bg, setBg] = useState(0) // ID de la quête en cours
-  page >= 0 && setQuest(page)
-  page === -1 && setPage(quest)
+  // -5 = Game Launch [Setup]// ID de la quête en cours
+  // const [quest, setQuest] = useState(quests[page])
+  const [bg, setBg] = useState(0)
 
-  useState(() => {
+  useEffect(() => {
+    // page >= 0 && setQuest(quests[page])
+    // page === -1 && setPage(quest)
     if (page >= 0) {
-      setBg()
+      // setBg(quests[quest].image)
     } else if (page === -1) {
-      setBg(tavernImg)
+      setBg()
     } else if (page === -2) {
       setBg(tavernImg)
     } else if (page === -3) {
       setBg()
     } else if (page === -4) {
+      setBg()
+    } else {
       setBg()
     }
   }, [page])
@@ -33,7 +36,15 @@ const Game = () => {
     <div className='game' style={{ backgroundImage: `url(${bg})` }}>
       {/* <Header /> */}
       {page === -2 && <GameTavern />}
-      {/* page >= 0 && <GameStory quest={quest} /> */}
+      {/* page >= 0 && <GameStory quest={quest} setPage={setPage} /> */}
+      <div>
+        <a
+          style={{ margin: 0, textAlign: 'center', lineHeight: '40px' }}
+          href={'https://fr.freepik.com'}
+        >
+          Illustré par upklyak - fr.freepik.com
+        </a>
+      </div>
     </div>
   )
 }
