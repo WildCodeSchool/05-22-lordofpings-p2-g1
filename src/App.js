7ea34f5020bdc48d04a4a650f35627e5c2fb52b3
LAURE CARILLO <laure.carillo@gmail.com>
2022-06-16 15:53:15 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import logo from './logo.svg'
+import Home from './pages/Home'
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.yozz
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn Reactsccc
-        </a>
-      </header>
+      <Home />
     </div>
   )
 }
