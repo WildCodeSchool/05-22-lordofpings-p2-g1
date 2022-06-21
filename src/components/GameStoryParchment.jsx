5afc3bdef334b4edbc2c9e3441fc20ca5afccf9a
kirijeremy <kirijeremy@gmail.com>
2022-06-21 12:52:50 +0200

    add a useState text and a useEffect to reload parchment animation

--- src/components/GameStoryParchment.jsx
+++ src/components/GameStoryParchment.jsx
@@ -1,11 +1,20 @@
 import parchmentLeft from '../assets/img/ui/left.svg'
 import parchmentRight from '../assets/img/ui/right.svg'
+import { useEffect } from 'react'
 
-const GameStoryParchment = ({ quest, animation }) => {
+const GameStoryParchment = ({ quest, animation, setAnimation }) => {
+  useEffect(() => {
+    setAnimation(true)
+  }, [quest])
+
+  useEffect(() => {
+    animation && setTimeout(() => setAnimation(false), 4000)
+  }, [animation])
+
   return (
     <div className='gameStoryParchment'>
       <img src={parchmentLeft} className='parchmentLeft'></img>
-      <div className='text' animation={animation && animation.toString()}>
+      <div className='text' animation={animation.toString()}>
         <p>{quest?.text}</p>
       </div>
       <img src={parchmentRight} className='parchmentRight'></img>
--- src/pages/Concept.jsx
+++ src/pages/Concept.jsx
@@ -1,30 +1,39 @@
 import { useNavigate } from 'react-router-dom'
 import Parchment from '../components/GameStoryParchment'
 import BigButton from '../components/BigButton'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const Concept = () => {
   const [para, setPara] = useState(true)
+  const [animation, setAnimation] = useState(true)
   const navigate = useNavigate()
 
   const concept = {
-    text: ' Le Déclin des Âmes est une histoire interactive inspirée de la série “Un livre dont vous êtes le héros” de Gallimard ainsi que des jeux de rôle.   Le Déclin des Âmes prend place au sein du mythe du Roi Arthur. L’enchanteur Merlin est devenu fou et tourmente tout un village : il a créé des golems de pierre, les Témoins de Merlin, qui harcèlent la population pour leur parler de la grandeur de leur maître et leur vendre des babioles. Vous êtes une aventurière déterminée à mettre Merlin hors d’état de nuire (parce que ça rapporte du pognon).'
+    text: 'Le Déclin des Âmes est une histoire interactive inspirée de la série “Un livre dont vous êtes le héros” de Gallimard ainsi que des jeux de rôle.   Le Déclin des Âmes prend place au sein du mythe du Roi Arthur. L’enchanteur Merlin est devenu fou et tourmente tout un village : il a créé des golems de pierre, les Témoins de Merlin, qui harcèlent la population pour leur parler de la grandeur de leur maître et leur vendre des babioles. Vous êtes une aventurière déterminée à mettre Merlin hors d’état de nuire (parce que ça rapporte du pognon).'
   }
 
   const rules = {
-    text: "Choisissez votre personnage, et avancez dans l’histoire en choisissant parmi les options proposées. Il n’y a pas de mauvais choix, seulement certaines conséquences douloureuses (la mort, ça compte comme une conséquence douloureuse). Vous aurez la possibilité d'acquérir de l’équipement et de participer à des mini-jeux à la taverne. Vos statistiques orienteront vos chances de réussite à certains tests critiques, mais ce sont surtout vos décisions qui détermineront l’histoire que vous vivrez."
+    text: 'Choisissez votre personnage, et avancez dans l’histoire en choisissant parmi les options proposées. Il n’y a pas de mauvais choix, seulement certaines conséquences douloureuses (la mort, ça compte comme une conséquence douloureuse). Vous aurez la possibilité d’acquérir de l’équipement et de participer à des mini-jeux à la taverne. Vos statistiques orienteront vos chances de réussite à certains tests critiques, mais ce sont surtout vos décisions qui détermineront l’histoire que vous vivrez.'
   }
-
-  function next() {
-    para ? setPara(!para) : navigate('../create')
-  }
+  const [text, setText] = useState(concept)
+  useEffect(() => (para === false ? setText(rules) : undefined), [para])
 
   return (
     <div className='concept'>
       <div className='conceptParchment'>
-        <Parchment quest={para ? concept : rules} />
+        <Parchment
+          quest={text}
+          animation={animation}
+          setAnimation={setAnimation}
+        />
       </div>
-      <BigButton onclick={next} text='Continuer !' height='100' />
+      <BigButton
+        onclick={() =>
+          animation === false && (para ? setPara(!para) : navigate('../create'))
+        }
+        text='Continuer'
+        height='100'
+      />
     </div>
   )
 }
