67588fe47046c18aacf4c3de82afb0d39b2ac7b4
flora cesarini <flo.cesarini@gmail.com>
2022-06-27 16:03:55 +0200

    start cssing contactform

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -15,46 +15,53 @@ const ContactForm = () => {
   console.log(`A ClientName has been entrered`)
 
   return (
-    <div>
-      <h1> Contactez-nous </h1>
-      <div className='formContainer'>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor='email'>Votre prénom : </label>
-          <br></br>
-          <input
-            type='text'
-            placeholder='Lola Fourth'
-            value={ClientName}
-            onChange={e => setClientName(e.target.value)}
-          />
-          <br></br>
-          <label htmlFor='email'>Votre email: </label>
-          <br></br>
-          <input
-            type='text'
-            placeholder='Lola4th@setmail.fr'
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-          <br></br>
-          <label htmlFor='selection'>
-            Choississez à qui vous souhaitez parler ! :
-          </label>
-          <select>
-            <option value='Romain'>Romain</option>
-            <option value='Remi'>Rémi</option>
-            <option value='Alexandra'>Alexandra</option>
-            <option value='Laure'>Laure</option>
-            <option value='Jérémy'>Jérémy</option>
-            <option value='Flora'>Flora</option>
-            <option value='Vincent'>Vincent</option>
-          </select>
-        </form>
+    <div className='grandmaContainer'>
+      <div className='motherContainer'>
+        <h1> Contactez-nous </h1>
+        <hr></hr>
+        <div className='formContainer'>
+          <form onSubmit={handleSubmit}>
+            <label className='labels' htmlFor='email'>
+              Votre prénom :{' '}
+            </label>
+            <br></br>
+            <input
+              type='text'
+              placeholder='Lola Fourth'
+              value={ClientName}
+              onChange={e => setClientName(e.target.value)}
+            />
+            <br></br>
+            <label className='labels' htmlFor='email'>
+              Votre email:{' '}
+            </label>
+            <br></br>
+            <input
+              type='text'
+              placeholder='Lola4th@setmail.fr'
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+            <br></br>
+            <label className='labels' htmlFor='selection'>
+              Choisissez à qui vous souhaitez parler ! :
+            </label>
+            <select>
+              <option value='Romain'>Romain</option>
+              <option value='Remi'>Rémi</option>
+              <option value='Alexandra'>Alexandra</option>
+              <option value='Laure'>Laure</option>
+              <option value='Jérémy'>Jérémy</option>
+              <option value='Flora'>Flora</option>
+              <option value='Vincent'>Vincent</option>
+            </select>
+          </form>
 
-        <br></br>
-        <input type='textarea' placeholder='Votre message ici !' />
-        <br></br>
-        <button onSubmit={handleSubmit}> Lancer son parchemin </button>
+          <br></br>
+          <input type='textarea' placeholder='Votre message ici !' />
+          <br></br>
+          <button onSubmit={handleSubmit}> Lancer son parchemin </button>
+        </div>
       </div>
     </div>
   )
