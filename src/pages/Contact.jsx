13becd49247123f1e46c26b880085ea494de80fc
alexandra panastier <panastier.alexandra@gmail.com>
2022-07-06 17:49:46 +0200

    REGARDE COMME CEST BEAU

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,18 +4,11 @@ import ContactForm from '../components/ContactForm'
 
 const Contact = () => {
   return (
-<<<<<<< HEAD
-    <div className='Background'>
-      <Team />
-      <ContactForm />
-
-=======
     <div>
       <div className='containerContact'>
         <Team />
         <ContactForm />
       </div>
->>>>>>> dev
       <Link to='/'>
         <button className='returnButton'>Retourner à la page principale</button>
       </Link>
